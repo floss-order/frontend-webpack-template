725931320e9cbc824ad177a5257ef1405e18ffca
floss-order <flossOrder@gmail.com>
2020-08-10 01:01:42 +0300

    Connected dist folder cleaner

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 
 module.exports = {
@@ -9,6 +10,7 @@ module.exports = {
     filename: './public/js/index.js'
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
         filename: 'index.html',
         template: './src/index.html'
