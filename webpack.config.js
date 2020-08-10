eeba27bc9babf5cc528745d7904d17b96b6ae6d8
floss-order <flossOrder@gmail.com>
2020-08-10 22:12:02 +0300

    Implement CSS and SASS support

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 
 module.exports = {
@@ -14,11 +15,28 @@ module.exports = {
     new HtmlWebpackPlugin({
         filename: 'index.html',
         template: './src/index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'public/css/[name].css'
     })
   ],
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     compress: false,
     port: 9000
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          'css-loader',
+          'sass-loader'
+        ],
+      },
+    ]
   }
 }
